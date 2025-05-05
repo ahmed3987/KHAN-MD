2a6225ef4ec7b989822fd7322f7dba24c5df4256
ahmed3987 <astff8690@gmail.com>
2025-05-05 06:47:29 +0500

    config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-// add your Session Id 
+// KHAN-MD~kWFTkDZY#uNR7u0Yqm7swaqH_CUs64Y10TSN51Bkx6JCPbRnTWqU
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
